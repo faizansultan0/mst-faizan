c5f52b14d6a868834194e075ef977746ece492cd
Sultan Faizan Bahoo <faizansultan113@gmail.com>
2024-04-22 21:55:28 +0500

    Implemented Entries Deletion

--- ArraysObjectsPracticeTask/index.js
+++ ArraysObjectsPracticeTask/index.js
@@ -16,6 +16,14 @@ let data = [
         joiningDate: '12-07-2020'
     },
     {
+        name: "Maaz",
+        email: "maaz@gmail.com",
+        dob: '23-1-2000',
+        experienceYears: 10,
+        department: 'IT',
+        joiningDate: '13-2-2014'
+    },
+    {
         name: 'Hasan',
         email: 'hasan@gmail.com',
         dob: '23-03-2004',
@@ -33,11 +41,13 @@ let data = [
     },
 ];
 
+// Printing Initial Values
 console.log('\nData is: ');
 for(let i=0;  i< data.length; i++) {
     console.log('Name: ' + data[i].name + ', Experience: ' + data[i].experienceYears)
 }
 
+// Sorting in Descending Order
 let m = 0;
 for(let i=0; i < data.length; i++){
     for(j=0; j < data.length; j++){
@@ -49,7 +59,36 @@ for(let i=0; i < data.length; i++){
     }
 }
 
+// Printing Values after sorting data
 console.log('Data after sorting wrt experience is: ');
 for(let i=0;  i< data.length; i++) {
+    console.log('Name: ' + data[i].name + ', Experience: ' + data[i].experienceYears)
+}
+
+
+// Deleting entries having experience greater than 2
+let k = 0, n = 0;
+for(let i=0; i < data.length; i++) {
+    if(data[i].experienceYears > 2) {
+        n = i;
+        while(n < data.length) {
+            if(n != data.length - 1){
+                data[n] = data[n+1];
+            }
+            n++; 
+        }
+        data[data.length - 1] = null;
+        data.length--;
+
+        // Rechecking if at the same index the new entry's experience is also greater than 2
+        if(data[i].experienceYears > 2) {
+            i--;
+        }
+    }
+}
+
+// Printing Final Values
+console.log('Data after deleting elements having experience greater than 2 is: ');
+for(let i = 0;  i < data.length; i++) {
     console.log('Name: ' + data[i].name + ', Experience: ' + data[i].experienceYears)
 }
