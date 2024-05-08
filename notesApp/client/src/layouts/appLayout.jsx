d11cc8e50aee78027360ed694bb48836595e2c2e
Sultan Faizan Bahoo <faizansultan113@gmail.com>
2024-05-08 22:44:24 +0500

    Scroller issue Resolved

--- notesApp/client/src/layouts/appLayout.jsx
+++ notesApp/client/src/layouts/appLayout.jsx
@@ -2,10 +2,10 @@ import Header from "../components/header/header";
 
 const AppLayout = ({children}) => {
     return (
-        <>
+        <div style={{minHeight: '100vh'}}>
             <Header />
             <main>{children}</main>
-        </>
+        </div>
     );
 };
 
