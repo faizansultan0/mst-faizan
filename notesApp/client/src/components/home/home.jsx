668de30432b9e9ee786f4da159b94b4e8aeda0da
Sultan Faizan Bahoo <faizansultan113@gmail.com>
2024-05-18 17:36:50 +0500

    Added delete note functionality on home page

--- notesApp/client/src/components/home/home.jsx
+++ notesApp/client/src/components/home/home.jsx
@@ -1,7 +1,8 @@
 import UserRoute from "../../routes/userRoute";
 import AppLayout from "../../layouts/appLayout";
-import { Container, Row, Col, Card, Pagination, Form, } from "react-bootstrap";
+import { Container, Row, Col, Card, Pagination, Form } from "react-bootstrap";
 import { useContext, useEffect, useState } from "react";
+import TrashIcon from "../../assets/trash-icon.svg";
 import { UserContext } from "../../context";
 import { Link } from "react-router-dom";
 import { toast } from "react-toastify";
@@ -15,92 +16,116 @@ const Home = () => {
     const [state] = useContext(UserContext);
 
     // Search Notes
-    const [searchQuery, setSearchQuery] = useState('');
+    const [searchQuery, setSearchQuery] = useState("");
 
     const items = [];
     for (let number = 1; number <= Math.ceil(totalNotes / 8); number++) {
         items.push(
-            <Pagination.Item key={number} active={number === active} onClick={() => setActive(number)}>
+            <Pagination.Item
+                key={number}
+                active={number === active}
+                onClick={() => setActive(number)}
+            >
                 {number}
             </Pagination.Item>
         );
     }
 
-    useEffect(() => {
-        const getAllNotes = async () => {
-            try {
-                const { data } = await axios.get(
-                    `${process.env.REACT_APP_SERVER_URL}/note/all?page=${active}`
-                );
-                if (data.error) {
-                    toast.error(data.error);
-                } else {
-                    // console.log(data);
-                    setNotes(data.notes);
-                }
-            } catch (err) {
-                console.log("An error occured while getting all notes: ", err);
-                toast.error("An error occured while getting all notes");
+    const getAllNotes = async () => {
+        try {
+            const { data } = await axios.get(
+                `${process.env.REACT_APP_SERVER_URL}/note/all?page=${active}`
+            );
+            if (data.error) {
+                toast.error(data.error);
+            } else {
+                // console.log(data);
+                setNotes(data.notes);
             }
-        };
+        } catch (err) {
+            console.log("An error occured while getting all notes: ", err);
+            toast.error("An error occured while getting all notes");
+        }
+    };
 
+    useEffect(() => {
         if (state && state.token && !searchQuery) getAllNotes();
     }, [state, active, searchQuery]);
 
-    useEffect(() => {
-        const getTotalNotes = async () => {
-            try {
-                const { data } = await axios.get(
-                    `${process.env.REACT_APP_SERVER_URL}/note/total`
-                );
-                if (data.error) {
-                    toast.error(data.error);
-                } else {
-                    // console.log(data);
-                    setTotalNotes(data.totalNotes);
-                    // console.log('Total Notes: ', data.totalNotes);
-                }
-            } catch (err) {
-                console.log(
-                    "An error occured while getting total notes: ",
-                    err
-                );
-                toast.error("An error occured while getting total notes");
+    const getTotalNotes = async () => {
+        try {
+            const { data } = await axios.get(
+                `${process.env.REACT_APP_SERVER_URL}/note/total`
+            );
+            if (data.error) {
+                toast.error(data.error);
+            } else {
+                // console.log(data);
+                setTotalNotes(data.totalNotes);
+                // console.log('Total Notes: ', data.totalNotes);
             }
-        };
+        } catch (err) {
+            console.log(
+                "An error occured while getting total notes: ",
+                err
+            );
+            toast.error("An error occured while getting total notes");
+        }
+    };
 
+    useEffect(() => {
         if (state && state.token && !searchQuery) getTotalNotes();
     }, [state, searchQuery]);
 
     const getSearchedNotes = async () => {
+        if (!searchQuery) return;
         try {
-            const { data } = await axios.get(`${process.env.REACT_APP_SERVER_URL}/note/search/${searchQuery}/${active}`);
+            const { data } = await axios.get(
+                `${process.env.REACT_APP_SERVER_URL}/note/search/${searchQuery}/${active}`
+            );
             if (data.error) {
                 toast.error(data.error);
             } else {
                 setNotes(data.notes);
                 setTotalNotes(data.total);
             }
-
         } catch (err) {
             console.log(`Search Query ERR: `, err);
         }
-    }
+    };
 
     useEffect(() => {
-        if (state && state.token && searchQuery) {
+        if (state && state.token) {
             setActive(1);
-            getSearchedNotes()
-        };
-    }, [state, searchQuery])
+            getSearchedNotes();
+        }
+    }, [state, searchQuery]);
 
     useEffect(() => {
-        if(searchQuery)getSearchedNotes();
+        getSearchedNotes();
     }, [active]);
 
     const searchHandle = (e) => {
         setSearchQuery(e.target.value);
-    }
+    };
+
+    const deleteHandle = async (nId) => {
+        try {
+            const { data } = await axios.delete(
+                `${process.env.REACT_APP_SERVER_URL}/note/delete/${nId}`
+            );
+            if (data.error) {
+                toast.error(data.error);
+            } else {
+                toast.success(data.message);
+                getAllNotes();
+                getTotalNotes();
+            }
+        } catch (error) {
+            console.log("Error Occured while deleting note: ", error);
+            toast.error("Could not delete note! Try again");
+        }
+    };
 
     return (
         <UserRoute>
@@ -172,6 +197,20 @@ const Home = () => {
                                                         </Card.Text>
                                                     </Card>
                                                 </Link>
+                                                        <button
+                                                            type="button"
+                                                            className="trash-btn bg-danger"
+                                                            onClick={() =>
+                                                                deleteHandle(
+                                                                    note._id
+                                                                )
+                                                            }
+                                                        >
+                                                            <img
+                                                                src={TrashIcon}
+                                                                alt="Trash Icon"
+                                                            />
+                                                        </button>
                                             </div>
                                         </Col>
                                     ))}
